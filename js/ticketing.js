e6f077004316b595925fc45f61894ec9cb5cc64c
panwoo1 <66258691+panwoo1@users.noreply.github.com>
2021-11-13 21:48:49 +0900

    ticket e

---
+++ js/ticketing.js
@@ -0,0 +1,8 @@
+function setData(movie, date, time, num, price) {
+    localStorage.setItem('selectedMovie', movie)
+    localStorage.setItem('selectedDate', date)
+    localStorage.setItem('selectedTime', time)
+    localStorage.setItem('selectedNum', num)
+    localStorage.setItem('selectedPrice', price)
+}
+
